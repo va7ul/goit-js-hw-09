51749e8e036ea045140d36c05cb9d69256944c5d
va7ul <vas7ul@gmail.com>
2023-06-26 14:16:31 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+startEl.addEventListener('click', () => {
+    startEl.setAttribute('disabled', 'disabled');
+
+    timerId = setInterval(() => {
+        let backgroundColor = getRandomHexColor();
+        bodyEl.style.backgroundColor = backgroundColor;
+    }, 1000)
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+stopEl.addEventListener('click', () => {
+    clearInterval(timerId);
+    startEl.removeAttribute('disabled');
+});
