dc08ba3827e80e0199bf992d55c98156e7ee8383
va7ul <123837103+va7ul@users.noreply.github.com>
2023-06-27 21:26:11 +0000

    Deploying to gh-pages from @ va7ul/goit-js-hw-09@1ab3999b32547ea5bbebf276ef8831625b8ade5f 🚀

---
+++ 01-color-switcher.afaa0ab4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.afaa0ab4.js.map
