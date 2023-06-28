07f22cc3a850a2407fe9a7e12a732601e56f2efc
va7ul <123837103+va7ul@users.noreply.github.com>
2023-06-28 09:08:28 +0000

    Deploying to gh-pages from @ va7ul/goit-js-hw-09@180c89c2028830194a2cc7020cc62dd99317e640 🚀

--- 01-color-switcher.58a59ffc.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{t.setAttribute("disabled","disabled"),a=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.58a59ffc.js.map
